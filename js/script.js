2d988fc2c5f499e4401dfa6a697cdf1eadd51901
Roberto Roig Pedro <rroigpedro@gmail.com>
2019-03-05 20:06:59 +0100

    Primer Commit
    -Añadido el header, menu y contactar completados con diseño responsive.
    -Acerca de nosotros con diseño incompleto de responsive
    -FALTA:
        - Index
        - Proyectos

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//ALL: Cargar el header y footer
+$(function(){
+    $("#header").load("header.html"); 
+    $("#footer").load("footer.html");
+    
+});
+
+//ALL: Para que haga el efecto de aparición de inicio
+$(window).on('beforeunload', function(){
+    $("body").css("opacity", "0");
+});
+
+$(window).on('load', function(){
+    $("body").css("opacity", "1");
+});
+
+//ALL: Función para manejar el menu
+function showMenu(x) {
+    x.classList.toggle("change");
+    if($("#h_menu").is(":visible")) {
+        $("#h_menu").fadeOut(300);
+        $("#h_menu_bkg").fadeOut(300);
+        $('html, body').css({
+            overflow: 'auto',
+            height: 'auto'
+        });
+    }
+        
+    else {
+        $("#h_menu").fadeIn(300);
+        $("#h_menu_bkg").fadeIn(300);
+        $('html, body').css({
+            overflow: 'hidden',
+            height: '100%'
+        });
+    }
+    
+}
+
+//CONTACTO: Función para ajustar el cuadro de texto
+function textAreaAdjust(o) {
+  o.style.height = "1px";
+  o.style.height = (o.scrollHeight)+"px";
+}
