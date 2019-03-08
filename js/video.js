5147a74971b16d8a99d5b8762cc7d2cf88f67f44
Bea Ruiz <bea684@gmail.com>
2019-03-08 19:04:10 +0100

    send help

--- js/video.js
+++ js/video.js
@@ -1,14 +1,92 @@
-var vid = document.getElementById("myvideo");
 
 function playVideo(){
+    var vid = document.getElementById("myvideo");
     vid.play();
+    
+    move();
 }
 
 function pauseVideo(){
+    var vid = document.getElementById("myvideo");
     vid.pause();
 }
 
-function stop() {
-   mediaPlayer.pause();
-   mediaPlayer.currentTime = 0;
+function stopVideo() {
+    var vid = document.getElementById("myvideo");
+    vid.pause();
+    vid.currentTime = 0;
 }
+
+function maxvolumenVideo () {
+    var vid = document.getElementById("myvideo");
+    vid.volume += vid.volume == 1 ? 0 : 0.1;
+}
+
+function minvolumenVideo () {
+    var vid = document.getElementById("myvideo");
+    vid.volume -= (vid.volume == 0 ? 0 : 0.1);
+    vid.volume = parseFloat(vid.volume).toFixed(1);
+}
+
+function maxVideo(){
+    var vid = document.getElementById("myvideo");    
+    vid.webkitRequestFullScreen();
+}
+
+function onVideoSourceChanged()
+{
+	if(videoSourceRunning == true)
+	{
+		onButtonStopClickedVideo();
+		var source = document.getElementById(videoSourceId);
+		source.src = document.getElementById(videoSelectId).value;
+		video.load();
+		onButtonPlayClickedVideo();
+	}
+	else
+	{
+		onButtonStopClickedVideo();
+		var source = document.getElementById(videoSourceId);
+		source.src = document.getElementById(videoSelectId).value;
+		video.load();
+	}
+}
+
+function move() {
+    duration_vid = document.getElementById("myvideo").duration;
+    document.getElementById("myProgress").style.width = "40%";
+    
+    width = 1;
+
+    var elem = document.getElementById("myBar");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width +=0.01; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
+
+/**
+function progressbarVideo(){
+    seekbar = document.getElementById("progressbar");
+    //Event listener for the seek bar
+    seekBar.addEventListener("change", function() {
+        //Calculate new time
+        var newTime = video.duration * (seekBar.value / 100);
+
+        video.currentTime = newTime;
+    });
+
+    //As video progresses, seekBar moves forward
+    seekBar.addEventListener("timeupdate", function() {
+        var value = (100 / video.duration) * video.currentTime;
+        seekBar.value = value;
+    });
+
+}
+**/
